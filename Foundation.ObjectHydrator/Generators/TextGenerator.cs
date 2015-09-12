fcb89fd7d62ca4a08ed8704a04c5497d125d329f
Jeremy Cook <jeremycook@users.noreply.github.com>
2015-09-12 11:23:07 -0600

    Constraining Length in setter instead of constructor
    This avoids Length unexpectedly being longer than sampleText.

--- Foundation.ObjectHydrator/Generators/TextGenerator.cs
+++ Foundation.ObjectHydrator/Generators/TextGenerator.cs
@@ -11,7 +11,12 @@ namespace Foundation.ObjectHydrator.Generators
         static string sampleText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non enim felis. Donec et leo vel lacus fermentum luctus ut in metus. Vivamus sagittis lacus vel mi sagittis sit amet molestie eros faucibus. Maecenas diam metus, scelerisque sed imperdiet nec, dignissim in dui. Aliquam erat volutpat. Maecenas malesuada gravida leo ac porttitor. Aliquam sed purus sit amet nisl ultrices accumsan at non ante. Duis lobortis, leo et viverra vestibulum, eros metus imperdiet justo, vel feugiat mi metus suscipit enim. Donec sed dui mi, vehicula malesuada leo. In pellentesque velit et diam aliquam vel facilisis metus faucibus. Curabitur a ipsum nulla. Suspendisse vel mi vel lacus fermentum rhoncus eget vestibulum ante. Morbi dictum sem id dui vulputate bibendum. Fusce quis faucibus leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse at malesuada mi.";
         Random random;
 
-        public int Length { get; set; }
+        private int _Length;
+        public int Length
+        {
+            get { return _Length; }
+            set { _Length = value > sampleText.Length ? sampleText.Length : value; }
+        }
 
         public TextGenerator()
             : this(25)
@@ -22,10 +27,6 @@ namespace Foundation.ObjectHydrator.Generators
         public TextGenerator(int length)
         {
             Length = length;
-            if (Length>sampleText.Length)
-            {
-                Length=sampleText.Length;
-            }
             random = RandomSingleton.Instance.Random;
         }
 
