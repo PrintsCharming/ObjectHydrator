0a8b22750d8f1ae3e329dab4282fff8ccf57806f
Дмитрий Ушенко <dmitry.ushenko@redkassa.ru>
2018-03-22 12:23:54 +0300

    CultureInfo properties generator

--- Foundation.ObjectHydrator.Tests/GeneratorTests/Generator_Tests.cs
+++ Foundation.ObjectHydrator.Tests/GeneratorTests/Generator_Tests.cs
@@ -369,5 +369,17 @@ namespace Foundation.ObjectHydrator.Tests.GeneratorTests
             var county = (string)unitedKingdomCountyGenerator.Generate();
             Assert.IsNotNull(county);
         }
+
+        [Test]
+        public void CultureInfoNamesGeneratorTest()
+        {
+            IGenerator<string> cultureDisplayNameGenerator = new CulturesGenerator<string>(culture => culture.DisplayName);
+            var cultureDisplayName = cultureDisplayNameGenerator.Generate();
+            Assert.IsNotNull(cultureDisplayName);
+
+            IGenerator<int> cultureLCIDGenerator = new CulturesGenerator<int>(culture => culture.LCID);
+            var cultureLCID = cultureLCIDGenerator.Generate();
+            Assert.Greater(cultureLCID, 0);
+        }
     }
 }
